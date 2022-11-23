159bccb9a54d84e052de23219d58efdf536fa8ea
Yangna <yangna1943@outlook.com>
2022-11-23 08:24:16 +0800

    实现了上下文菜单“删除”功能

--- app/src/main/java/com/jnu/bmapplication/MainActivity.java
+++ app/src/main/java/com/jnu/bmapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.jnu.bmapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +22,11 @@ import com.jnu.bmapplication.Data.Book;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainRecycleViewAdapter mainRecycleViewAdapter;
+    private static final int MENU_ID_ADD = 1;
+    private static final int MENU_ID_DELETE = 2;
+    private static final int MENU_ID_UPDATE = 3;
 
     private ArrayList<Book>books;
     @Override
@@ -32,26 +43,51 @@ public class MainActivity extends AppCompatActivity {
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerViewMain.setLayoutManager(linearLayoutManager);
         //接收数据
-        MainRecycleViewAdapter mainRecycleViewAdapter=new MainRecycleViewAdapter(books);
+        mainRecycleViewAdapter=new MainRecycleViewAdapter(books);
         recyclerViewMain.setAdapter(mainRecycleViewAdapter);
+
     }
+    //ContextMenu实现
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case MENU_ID_DELETE:
+                AlertDialog alertDialog;
+                alertDialog=new AlertDialog.Builder(this)
+                        .setTitle(R.string.confirmation)
+                        .setMessage(R.string.sure_to_delete)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                books.remove(item.getOrder());
+                                mainRecycleViewAdapter.notifyItemRemoved(item.getOrder());
+                            }
+                        }).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).create();
+                alertDialog.show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
     //调节器代码
-    public class MainRecycleViewAdapter extends RecyclerView.Adapter<MainRecycleViewAdapter.ViewHolder> {
+    public class MainRecycleViewAdapter extends RecyclerView.Adapter<MainRecycleViewAdapter.ViewHolder>  {
 
         private ArrayList<Book> localDataSet;
 
-        /**
-         * Provide a reference to the type of views that you are using
-         * (custom ViewHolder).
-         */
-        public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
 
             private final TextView textView;
             private final TextView textView1;
             private final TextView textView2;
             private final TextView textView3;
             private final ImageView imageView;
-
             public ViewHolder(View view) {
                 super(view);
                 // Define click listener for the ViewHolder's View
@@ -60,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
                 textView1=(TextView) view.findViewById(R.id.ap);
                 textView2=(TextView) view.findViewById(R.id.time);
                 textView3=(TextView) view.findViewById(R.id.publi);
+
+                view.setOnCreateContextMenuListener(this);
             }
 
             public TextView getTextView() {
@@ -79,14 +117,17 @@ public class MainActivity extends AppCompatActivity {
             public ImageView getImageView() {
                 return imageView;
             }
+
+
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                contextMenu.add(0,MENU_ID_UPDATE,getAdapterPosition(),"Update ");
+                contextMenu.add(0,MENU_ID_DELETE,getAdapterPosition(),"Delete ");
+
+            }
         }
 
-        /**
-         * Initialize the dataset of the Adapter.
-         *
-         * @param dataSet String[] containing the data to populate views to be used
-         *                by RecyclerView.
-         */
+
         public MainRecycleViewAdapter(ArrayList<Book> dataSet) {
             localDataSet = dataSet;
         }
@@ -107,10 +148,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(ViewHolder viewHolder, final int position) {
 
-            // Get element from your dataset at this position and replace the
-            // contents of the view with that element
-
-
             viewHolder.getTextView().setText(localDataSet.get(position).getTitle());
             viewHolder.getTextView1().setText(localDataSet.get(position).getAp());
             viewHolder.getTextView2().setText(localDataSet.get(position).getTime());
@@ -126,6 +163,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getBook(){
-        books.add(new Book("数学","陈天 ","清华大学出版社","2002-0905",R.drawable.book_1));
+        books.add(new Book("人生海海","麦家 ","北京十月文艺出版社","2019-04",R.drawable.book_1));
+        books.add(new Book("兄弟","余华","作家出版社","2012-09",R.drawable.book_1));
+        books.add(new Book("三体","刘慈欣","电子影音出版社","2018-12",R.drawable.book_1));
+        books.add(new Book("命运","蔡崇达","广州出版社","2022-09",R.drawable.book_1));
+        books.add(new Book("白鹿原","陈忠实","人民文学出版社","2010-07",R.drawable.book_1));
+        books.add(new Book("我与地坛","史铁生","人民文学出版社","2011-04",R.drawable.book_1));
     }
 }
